dc4055363cbdf67f705d5e8ad15f3e39ca60f8a6
Kostushka <nastyatery47@yandex.ru>
2021-11-29 14:13:22 +0300

    Added basename for gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import { REPO_NAME } from './constants/repo';
 import Router from './containers/Router';
 import { AuthContext } from './context';
 
@@ -15,7 +16,7 @@ const App = () => {
     }, []);
     return (
         <AuthContext.Provider value={{ isAuth, setIsAuth, isLoading }}>
-            <BrowserRouter>
+            <BrowserRouter basename={`/${REPO_NAME}/`}>
                 <Navigation />
                 <Router />
             </BrowserRouter>
